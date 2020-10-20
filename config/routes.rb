812ff1456d720c3abe542af691e73d4582c705de
Aline <alinecris@gmail.com>
2020-10-20 17:46:08 +0200

    Create ratings controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :stores, only: [:index, :show], :defaults => { :format => 'json' }
+      resources :ratings, only: [:create], :defaults => { :format => 'json' }
     end
   end
 end
