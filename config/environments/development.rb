8b5d929efc6b3a77a535a3c5153be9b10dafec0d
Aline <alinecris@gmail.com>
2020-10-24 18:55:52 +0200

    Adjust development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  Rails.application.configure do
+    config.hosts.clear
+  end
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
